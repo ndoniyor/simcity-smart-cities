0c6efc8ae7ba90c683238f699244667a307adc49
ndoniyor <ndoniyor123@gmail.com>
2023-12-03 21:32:17 -0500

    added more helper functions/constants

---
+++ src/utils/constants.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+class Paths:
+    ROOT_PATH = Path(__file__).resolve().parent.parent.parent
+    SRC_PATH = ROOT_PATH / "src"
+    DATA_PATH = ROOT_PATH / "data"
+
+    PROCESSED_DATA_BASE = DATA_PATH / "processed"
+    PROCESSED_DATA_CSV = PROCESSED_DATA_BASE / "csv"
+    PROCESSED_DATA_XLSX = PROCESSED_DATA_BASE / "xlsx"
+    PROCESSED_DATA_PARQUET = PROCESSED_DATA_BASE / "parquet"
+
+    RAW_DATA_BASE = DATA_PATH / "raw"
+    RAW_DATA_CSV = RAW_DATA_BASE / "csv"
+    RAW_DATA_XLSX = RAW_DATA_BASE / "xlsx"
+    RAW_DATA_PARQUET = RAW_DATA_BASE / "parquet"
+
+
+class Urls:
+    OPEN_DATA_URL = "data.cityofnewyork.us"
+    ENDPOINT_311 = "erm2-nwe9"
+    ENDPOINT_HOUSING_CODE = "wvxf-dwi5"
+    ENDPOINT_PARKING = "nc67-uf89"
+    ENDPOINT_VACANT_LOT = "r4c5-ndkx"
--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,10 +1,39 @@
-import pandas as pd 
+from pyspark.sql import SparkSession
+import pandas as pd
+from datetime import datetime
+
+from utils.constants import Paths
+
 
 def create_yearly_query(year, year_field):
     start_of_year = f"{year}-01-01T00:00:00"
     end_of_year = f"{year+1}-01-01T00:00:00"
     query = [f"{year_field} >= '{start_of_year}'", f"{year_field} < '{end_of_year}'"]
     return query
+
+
+def create_monthly_query(year_month, date_col):
+    start_of_month = f"{year_month}-01T00:00:00"
+    end_of_month = (
+        datetime.strptime(start_of_month, "%Y-%m-%dT%H:%M:%S") + pd.offsets.MonthEnd()
+    ).strftime("%Y-%m-%dT%H:%M:%S")
+    query = [f"{date_col} >= '{start_of_month}'", f"{date_col} < '{end_of_month}'"]
+    return query
+
+
+def filter_parquet(
+    file_name: str, columns: list[str] = [], filters: list[str] = []
+) -> pd.DataFrame:
+    spark = SparkSession.builder.appName("Parquet Worker").getOrCreate()
+    df = spark.read.parquet(str(Paths.RAW_DATA_PARQUET / f"{file_name}.parquet"))
+    for condition in filters:
+        df = df.filter(condition)
+    if columns:
+        df = df.select(*columns)
+    df = df.toPandas()
+    spark.stop()
+    return df
+
 
 def compute_filesize(df):
     return (df.memory_usage(deep=True).sum()) / (1024**2)
