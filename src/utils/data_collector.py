495db7f67bb63d73f6ab19bdfecef24523a97b3e
ndoniyor <ndoniyor123@gmail.com>
2023-12-03 21:32:27 -0500

    undo changes to collector

--- src/utils/data_collector.py
+++ src/utils/data_collector.py
@@ -1,17 +1,12 @@
 from os import getenv
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from pandas import DataFrame, concat, read_csv
+from pandas import DataFrame, concat
 from dotenv import load_dotenv
 from sodapy import Socrata
 
 from utils import helpers
-
-DOMAIN = "data.cityofnewyork.us"
-CSV_FILE_PATH = "../../data/raw/"
-
-load_dotenv(dotenv_path="../../.env")
-app_token = getenv("SOCRATA_TOKEN")
+from utils.constants import Paths, Urls
 
 
 class DataCollector:
@@ -20,7 +15,13 @@ class DataCollector:
         self.filters = filters
         self.columns = columns
         self.date_col = date_col
-        self.client = Socrata(domain=DOMAIN, app_token=app_token, timeout=10)
+
+        load_dotenv(dotenv_path=Paths.ROOT_PATH / ".env")
+        app_token = getenv("SOCRATA_TOKEN")
+
+        self.client = Socrata(
+            domain=Urls.OPEN_DATA_URL, app_token=app_token, timeout=10
+        )
         self.df = DataFrame()
 
     def _collect_data_for_year(self, year) -> DataFrame:
@@ -47,12 +48,12 @@ class DataCollector:
 
         return concat(dfs, ignore_index=True)
 
-    def collect_data(self, years=[]) -> None:
+    def collect_data(self, year_range) -> None:
         start = time.time()
-        with ThreadPoolExecutor(max_workers=len(years)) as executor:
+        with ThreadPoolExecutor(max_workers=len(year_range)) as executor:
             future_to_year = {
                 executor.submit(self._collect_data_for_year, year): year
-                for year in years
+                for year in year_range
             }
             dfs = []
             for future in as_completed(future_to_year):
@@ -65,16 +66,10 @@ class DataCollector:
 
         self.df = concat(dfs, ignore_index=True) if dfs else DataFrame()
         end = time.time()
-        print(
-            f"""
-            File size: {helpers.compute_filesize(self.df)} MB
-            Operation took {end- start} seconds
-            """
-        )
-
-    def read_data(self, name) -> None:
-        file_path = CSV_FILE_PATH + name + ".csv"
-        self.df = read_csv(file_path)
+        print(f"Operation took {end - start} seconds")
 
     def to_csv(self, file_name) -> None:
-        self.df.to_csv(CSV_FILE_PATH + file_name, index=False)
+        self.df.to_csv(Paths.RAW_DATA_CSV / file_name, index=False)
+
+    def to_parquet(self, file_name) -> None:
+        self.df.to_parquet(Paths.RAW_DATA_CSV / file_name, index=False)
